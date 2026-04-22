0dc709c93c2c2e84c4f7c41c5be98beaf2a4e67f
hemriti amine <hemritimahamedamine@fss.u-sfax.tn>
2026-04-22 22:50:28 +0100

    fix

--- actions/connexion.php
+++ actions/connexion.php
@@ -5,7 +5,7 @@ $user = "root";
 $pass = "";
 
 try {
-    $cnx = new PDO("mysql:host=$host;dbname=$base", $user, $pass);
+    $cnx = new PDO("mysql:host=$host;dbname=$base;charset=utf8mb4", $user, $pass);
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
